72e15b50080c3cd86c8c71c39993aa1d9d0a3edf
Venkatesan Srinivasan <vvs@tnineworks.com>
2020-02-03 19:00:23 +0530

    Using flex box instead of floats

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Classifier from './Classifier';
 
 function App() {
   return (
@@ -20,19 +21,22 @@ class AppHeader extends React.Component {
   render() {
     return (
       <div className="AppHeader">
-        <img src={logo} style={{float: "left", width: "5em"}} />
-        <div style={{float:"right"}}>
+        <img src={logo} style={{width:'5em'}} />
+        <div>
             <span className="ProfileButton">Guest</span>
             <span>{getTimestamp()}</span>
         </div>
-        <div className="ClearFloat"></div>
       </div>
     );
   }
 }
 
 class AppBody extends React.Component {
-
+    render() {
+        return (
+            <Classifier />
+        );
+    }
 }
 
 
--- src/Classifier.js
+++ src/Classifier.js
@@ -1,3 +1,4 @@
+import React from 'react';
 
 class InputString extends React.Component {
 
