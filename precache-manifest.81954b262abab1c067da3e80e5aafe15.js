0ccd99fc36f771a0601a6fed4f7a425d2df0b861
Venkatesan Srinivasan <vvs@tnineworks.com>
2020-04-17 22:11:50 +0530

    Updates

---
+++ precache-manifest.81954b262abab1c067da3e80e5aafe15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e943ecc22806937d86137706f1e1974",
+    "url": "/vagaipaduththal/index.html"
+  },
+  {
+    "revision": "ab43c1a053fdb63e42f3",
+    "url": "/vagaipaduththal/static/css/main.7a69232a.chunk.css"
+  },
+  {
+    "revision": "8a835913a20ded7d88b9",
+    "url": "/vagaipaduththal/static/js/2.ad64474e.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/vagaipaduththal/static/js/2.ad64474e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab43c1a053fdb63e42f3",
+    "url": "/vagaipaduththal/static/js/main.838d1230.chunk.js"
+  },
+  {
+    "revision": "64b524cf2c16e1c1a33f",
+    "url": "/vagaipaduththal/static/js/runtime-main.4d1deaa5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vagaipaduththal/static/media/logo.5d5d9eef.svg"
+  }
+]);
