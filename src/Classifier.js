5d531c88bb416a0f6136e6566ba5994668f48e20
Venkatesan Srinivasan <vvs@tnineworks.com>
2025-06-22 23:38:29 +0530

    updating selected IDs to DB

--- src/Classifier.js
+++ src/Classifier.js
@@ -4,8 +4,11 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
 import CreateMember from './CreateMember'
 import Suggestions from './Suggestions'
+import axios from 'axios'
 
 class InputString extends React.Component {
     // constructor(props) {
@@ -31,7 +34,11 @@ class Classifier extends React.Component {
             activePage: 1,
             selectedmid: [],
             gotoPage: '',
-            sortedInputStrings: this.sortInputStrings(props.inputStrings, props.taggedData)
+            sortedInputStrings: this.sortInputStrings(props.inputStrings, props.taggedData),
+            isUpdating: false,
+            updateSuccess: false,
+            updateError: null,
+            validationError: null
         }
     }
 
@@ -95,7 +102,7 @@ class Classifier extends React.Component {
         this.state.sortedInputStrings.forEach((element, index) => {
             const suggestions = this.props.taggedData[element];
             if (suggestions && suggestions.mid && suggestions.mid !== "na") {
-                selectedmidUpdated[index] = suggestions.mid;
+                selectedmidUpdated[index] = this.props.members[suggestions.mid];
             }
         });
         this.setState({ selectedmid: selectedmidUpdated });
@@ -114,6 +121,114 @@ class Classifier extends React.Component {
             alert('Invalid page number');
             this.setState({ gotoPage: '' });
         }
+    }
+
+    validateTransactionData = (transactionData, inputString) => {
+        if (!transactionData) {
+            return `Missing transaction data for: ${inputString}`;
+        }
+        
+        if (!transactionData.transactionAmount) {
+            return `Missing transaction amount for: ${inputString}`;
+        }
+        
+        if (!transactionData.transactionDescription) {
+            return `Missing transaction description for: ${inputString}`;
+        }
+        
+        if (!transactionData.transactionDate) {
+            return `Missing transaction date for: ${inputString}`;
+        }
+        
+        if (!transactionData.transactionId) {
+            return `Missing transaction ID for: ${inputString}`;
+        }
+        
+        return null;
+    }
+
+    handleUpdateDatabase = () => {
+        // Reset all status states
+        this.setState({ 
+            isUpdating: false, 
+            updateSuccess: false, 
+            updateError: null,
+            validationError: null
+        });
+        
+        // Validate all transaction data first
+        let validationError = null;
+        
+        for (let i = 0; i < this.state.sortedInputStrings.length; i++) {
+            const inputString = this.state.sortedInputStrings[i];
+            const transactionData = this.props.taggedData[inputString];
+            
+            const error = this.validateTransactionData(transactionData, inputString);
+            if (error) {
+                validationError = error;
+                break;
+            }
+        }
+        
+        if (validationError) {
+            this.setState({ validationError });
+            return;
+        }
+        
+        // Set updating state
+        this.setState({ isUpdating: true });
+
+        // Prepare data for the API call
+        const transactions = [];
+        
+        this.state.sortedInputStrings.forEach((inputString, index) => {
+            const transactionData = this.props.taggedData[inputString];
+            const memberInfo = this.state.selectedmid[index];
+            
+            // Create a transaction object with fields matching the deposits table
+            const transaction = {
+                // If a member is assigned, use their ID, otherwise set to null
+                member_id: memberInfo ? memberInfo.mid : null,
+                amount: transactionData.transactionAmount,
+                description: transactionData.transactionDescription,
+                date: transactionData.transactionDate,
+                type: 'REG', // Default type since it's required
+                bank_trans_id: transactionData.transactionId
+            };
+            
+            transactions.push(transaction);
+        });
+
+        // Create the final JSON structure to send to the backend
+        const requestData = {
+            transactions: transactions
+        };
+
+        console.log('Sending transaction data to backend:', requestData);
+
+        // Make API call to the backend
+        axios.post('http://localhost:8081/api/update-deposits', requestData)
+        .then(response => {
+            console.log('Database updated successfully:', response.data);
+            this.setState({ 
+                isUpdating: false, 
+                updateSuccess: true,
+                updateError: null
+            });
+            
+            // Hide success message after 5 seconds
+            setTimeout(() => {
+                this.setState({ updateSuccess: false });
+            }, 5000);
+        })
+        .catch(error => {
+            console.error('Error updating database:', error);
+            this.setState({ 
+                isUpdating: false,
+                updateSuccess: false,
+                updateError: error.response?.data?.message || 'An unknown error occurred'
+            });
+        });
     }
 
     render() {
@@ -155,6 +270,34 @@ class Classifier extends React.Component {
                     marginLeft: "1em"
                 }}>
                     Identified: {this.state.selectedmid.reduce((x, value) => typeof value !== "undefined" ? x + 1 : x, 0)} of {this.state.sortedInputStrings.length} total
+                    <div style={{ marginTop: "1em" }}>
+                        <Button 
+                            variant="success" 
+                            size="lg" 
+                            onClick={this.handleUpdateDatabase}
+                            disabled={this.state.isUpdating}
+                        >
+                            {this.state.isUpdating ? 'Updating...' : 'Update Database with Transaction Details'}
+                        </Button>
+                        
+                        {this.state.updateSuccess && 
+                            <Alert variant="success" className="mt-2">
+                                Database updated successfully!
+                            </Alert>
+                        }
+                        
+                        {this.state.updateError && 
+                            <Alert variant="danger" className="mt-2">
+                                Error updating database: {this.state.updateError}
+                            </Alert>
+                        }
+                        
+                        {this.state.validationError && 
+                            <Alert variant="warning" className="mt-2">
+                                {this.state.validationError}
+                            </Alert>
+                        }
+                    </div>
                 </div>
                 <div style={{
                     display: "inline-block",
